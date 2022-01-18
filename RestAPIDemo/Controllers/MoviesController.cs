73dd89fcb71beb441ed328b1abcf80c6ce07130c
bharath9788 <bhara@LAPTOP-LROAJC67>
2022-01-18 15:41:48 +0530

    Added Entity Framework

---
+++ RestAPIDemo/Controllers/MoviesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestAPIDemo.Model;
+
+namespace RestAPIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private static List<Movie> _movies = new List<Movie>()
+        {
+            new Movie { Title = "1stMovie", Id = 1 , Language = "French"},
+            new Movie { Title = "2ndMovie", Id = 2 , Language = "French"},
+        };
+
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            return _movies;
+        }
+
+
+        [HttpPost]
+        public void Post([FromBody] Movie newMovie)
+        {
+            _movies.Add(newMovie);
+        }
+
+        [HttpPut("{id:int}")]
+        public void Put(int id, [FromBody] Movie toUpdateMovie)
+        {
+            _movies[id] = toUpdateMovie;
+        }
+ 
+    }
+}
---
+++ RestAPIDemo/Data/AppDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestAPIDemo.Model;
+
+namespace RestAPIDemo.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+
+
+        }
+
+        public DbSet<Movie> movieDbSet { get; set;}
+}
+}
